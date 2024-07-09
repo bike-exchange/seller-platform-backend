e7ba39bc058d78ee54ce6ecea90fd0d02ead97f9
bex-ayoub <ayoub@bikeexchange.de>
2024-07-09 15:15:42 +0200

    fix(models): :bug: fix product-store relation

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, Index, JoinColumn, ManyToOne } from "typeorm";
+import { Entity, JoinTable, ManyToMany } from "typeorm";
 
 import { Product as MedusaProduct } from "@medusajs/medusa";
 
@@ -6,11 +6,7 @@ import { Store } from "./store";
 
 @Entity()
 export class Product extends MedusaProduct {
-  @Index("ProductStoreId")
-  @Column({ nullable: true })
-  store_id?: string;
-
-  @ManyToOne(() => Store, (store) => store.products)
-  @JoinColumn({ name: "store_id", referencedColumnName: "id" })
-  store?: Store;
+  @ManyToMany(() => Store, { cascade: true })
+  @JoinTable()
+  stores: Store[];
 }
--- src/services/product.ts
+++ src/services/product.ts
@@ -34,63 +34,63 @@ class ProductService extends MedusaProductService {
     }
   }
 
-  /**
-   * Assigns store_id to selector if not provided except for admin user who can see all users
-   * @param selector
-   */
-  private prepareListConfig_(
-    selector?: ProductSelector,
-    config?: FindProductConfig
-  ) {
-    selector = selector || {};
+  // /**
+  //  * Assigns store_id to selector if not provided except for admin user who can see all users
+  //  * @param selector
+  //  */
+  // private prepareListConfig_(
+  //   selector?: ProductSelector,
+  //   config?: FindProductConfig
+  // ) {
+  //   selector = selector || {};
 
-    const isAdminUser = checkIsAdminUser(this.loggedInUser_);
-    if (!isAdminUser && this.loggedInUser_?.store_id && !selector.store_id) {
-      selector.store_id = this.loggedInUser_.store_id;
-    }
-    config?.select?.push("store_id");
-    config?.relations?.push("store");
-  }
+  //   const isAdminUser = checkIsAdminUser(this.loggedInUser_);
+  //   if (!isAdminUser && this.loggedInUser_?.store_id && !selector.store_id) {
+  //     selector.store_id = this.loggedInUser_.store_id;
+  //   }
+  //   config?.select?.push("store_id");
+  //   config?.relations?.push("store");
+  // }
 
-  async list(
-    selector: ProductSelector,
-    config?: FindProductConfig
-  ): Promise<Product[]> {
-    this.prepareListConfig_(selector, config);
+  // async list(
+  //   selector: ProductSelector,
+  //   config?: FindProductConfig
+  // ): Promise<Product[]> {
+  //   this.prepareListConfig_(selector, config);
 
-    const products = await super.list(selector, config);
-    return products;
-  }
+  //   const products = await super.list(selector, config);
+  //   return products;
+  // }
 
-  async listAndCount(
-    selector: ProductSelector,
-    config?: FindProductConfig
-  ): Promise<[Product[], number]> {
-    this.prepareListConfig_(selector, config);
+  // async listAndCount(
+  //   selector: ProductSelector,
+  //   config?: FindProductConfig
+  // ): Promise<[Product[], number]> {
+  //   this.prepareListConfig_(selector, config);
 
-    const products = await super.listAndCount(selector, config);
-    return products;
-  }
+  //   const products = await super.listAndCount(selector, config);
+  //   return products;
+  // }
 
-  async create(productObject: CreateProductInput): Promise<Product> {
-    if (!productObject.store_id && this.loggedInUser_?.store_id) {
-      productObject.store_id = this.loggedInUser_.store_id;
-
-      // This will generate a handle for the product based on the title and store_id
-      // e.g. "sunglasses-01HXVYMJF9DW..."
-      const title = productObject.title
-        .normalize("NFD")
-        .replace(/[\u0300-\u036f]/g, "")
-        .replace(/\s+/g, "-")
-        .toLowerCase();
-      const store_id = this.loggedInUser_.store_id.replace("store_", "");
-
-      productObject.handle = `${title}-${store_id}`;
-    }
-
-    const product = await super.create(productObject);
-    return product;
-  }
+  //   async create(productObject: CreateProductInput): Promise<Product> {
+  //     if (!productObject.store_id && this.loggedInUser_?.store_id) {
+  //       productObject.store_id = this.loggedInUser_.store_id;
+  //
+  //       // This will generate a handle for the product based on the title and store_id
+  //       // e.g. "sunglasses-01HXVYMJF9DW..."
+  //       const title = productObject.title
+  //         .normalize("NFD")
+  //         .replace(/[\u0300-\u036f]/g, "")
+  //         .replace(/\s+/g, "-")
+  //         .toLowerCase();
+  //       const store_id = this.loggedInUser_.store_id.replace("store_", "");
+  //
+  //       productObject.handle = `${title}-${store_id}`;
+  //     }
+  //
+  //     const product = await super.create(productObject);
+  //     return product;
+  //   }
 }
 
 export default ProductService;
